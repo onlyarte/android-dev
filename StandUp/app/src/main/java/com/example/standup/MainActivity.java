5a664f7bee4789e4322c5636c001cd150dbe0d2d
Ruslan Purii <ruslanpurii@gmail.com>
2019-03-12 18:05:11 +0200

    hw 7, 8.1 & basis for 8.2

--- StandUp/app/src/main/java/com/example/standup/MainActivity.java
+++ StandUp/app/src/main/java/com/example/standup/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.standup;
 
+import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
@@ -13,10 +16,11 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    // Notification ID.
     private static final int NOTIFICATION_ID = 0;
+    // Notification channel ID.
     private static final String PRIMARY_CHANNEL_ID =
             "primary_notification_channel";
-
     private NotificationManager mNotificationManager;
 
     @Override
@@ -28,24 +32,65 @@ public class MainActivity extends AppCompatActivity {
                 getSystemService(NOTIFICATION_SERVICE);
 
         ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
-        alarmToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                String toastMessage;
-                if(isChecked){
-                    //Set the toast message for the "on" case.
-                    toastMessage = getString(R.string.toast_alarm_on);
-                } else {
-                    //Set the toast message for the "off" case.
-                    toastMessage = getString(R.string.toast_alarm_off);
-                }
 
-                //Show a toast to say the alarm is turned on or off.
-                Toast.makeText(MainActivity.this, toastMessage,Toast.LENGTH_SHORT)
-                        .show();
-            }
-        });
+        // Set up the Notification Broadcast Intent.
+        Intent notifyIntent = new Intent(this, AlarmReceiver.class);
 
+        boolean alarmUp = (PendingIntent.getBroadcast(this, NOTIFICATION_ID,
+                notifyIntent, PendingIntent.FLAG_NO_CREATE) != null);
+        alarmToggle.setChecked(alarmUp);
+
+        final PendingIntent notifyPendingIntent = PendingIntent.getBroadcast
+                (this, NOTIFICATION_ID, notifyIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
+        final AlarmManager alarmManager = (AlarmManager) getSystemService
+                (ALARM_SERVICE);
+
+        // Set the click listener for the toggle button.
+        alarmToggle.setOnCheckedChangeListener
+                (new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged
+                            (CompoundButton buttonView, boolean isChecked) {
+                        String toastMessage;
+                        if (isChecked) {
+
+                            long repeatInterval = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
+
+                            long triggerTime = SystemClock.elapsedRealtime()
+                                    + repeatInterval;
+
+                            // If the Toggle is turned on, set the repeating alarm with
+                            // a 15 minute interval.
+                            if (alarmManager != null) {
+                                alarmManager.setInexactRepeating
+                                        (AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                                                triggerTime, repeatInterval,
+                                                notifyPendingIntent);
+                            }
+                            // Set the toast message for the "on" case.
+                            toastMessage = getString(R.string.toast_alarm_on);
+
+                        } else {
+                            // Cancel notification if the alarm is turned off.
+                            mNotificationManager.cancelAll();
+
+                            if (alarmManager != null) {
+                                alarmManager.cancel(notifyPendingIntent);
+                            }
+                            // Set the toast message for the "off" case.
+                            toastMessage = getString(R.string.toast_alarm_off);
+
+                        }
+
+                        // Show a toast to say the alarm is turned on or off.
+                        Toast.makeText(MainActivity.this, toastMessage,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // Create the notification channel.
         createNotificationChannel();
     }
 
@@ -64,23 +109,17 @@ public class MainActivity extends AppCompatActivity {
                 android.os.Build.VERSION_CODES.O) {
 
             // Create the NotificationChannel with all the parameters.
-            NotificationChannel notificationChannel = new NotificationChannel(
-                    PRIMARY_CHANNEL_ID,
-                    "Stand up notification",
-                    NotificationManager.IMPORTANCE_HIGH
-            );
+            NotificationChannel notificationChannel = new NotificationChannel
+                    (PRIMARY_CHANNEL_ID,
+                            "Stand up notification",
+                            NotificationManager.IMPORTANCE_HIGH);
 
             notificationChannel.enableLights(true);
             notificationChannel.setLightColor(Color.RED);
             notificationChannel.enableVibration(true);
-            notificationChannel.setDescription("Notifies every 15 minutes to stand up and walk");
+            notificationChannel.setDescription("Notifies every 15 minutes to " +
+                    "stand up and walk");
             mNotificationManager.createNotificationChannel(notificationChannel);
         }
-    }
-
-    private void deliverNotification(Context context) {
-        Intent contentIntent = new Intent(context, MainActivity.class);
-        PendingIntent contentPendingIntent = PendingIntent.getActivity
-                (context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
